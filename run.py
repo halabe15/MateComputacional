1a034763063560b21044b1ea02fe3d0122bcb3bc
halabe15 <halabe15@gmail.com>
2018-03-30 12:06:21 -0600

    epsilon

--- run.py
+++ run.py
@@ -5,30 +5,37 @@ def run(automata, str):
     fin = automata['final']
     autom = automata['automata']
     agrega = True
+    tmp = []
 
 
     while agrega:
         agrega = False
         for state in stand:
-            if state not in stand:
-                stand.append(state)
             for ins in autom:
                 res = ins.checkEpsilon(state)
                 if res != None and res not in stand:
                     stand.append(res)
                     agrega = True
-    print(stand)
-
-    for state in stand:
-        for char in str:
-            for position in realStates:
+    for char in str:
+        for state in stand:
+            for ins in autom:
+                res = ins.indicate(state, char)
+                if res != None:
+                    tmp.append(res)
+        stand = tmp
+        tmp = []
+        agrega = True
+        while agrega:
+            agrega = False
+            for state in stand:
                 for ins in autom:
-                    res = ins.indicate(position, char)
-                    if res != None:
-                        # print(res)
+                    res = ins.checkEpsilon(state)
+                    if res != None and res not in stand:
                         stand.append(res)
-            # stand = tmp
-    tmp = []
+                        agrega = True
+
+
+
 
     for state in stand:
         if state not in tmp:
