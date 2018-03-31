5e6f174d83b52c609b83fb116e8f8012e4b18e5e
halabe15 <halabe15@gmail.com>
2018-03-30 18:10:45 -0600

    Comentarios

--- run.py
+++ run.py
@@ -6,32 +6,44 @@ def run(automata, str):
     initial = Stack()
     initial.push(automata['initial'][0])
     stand = [initial]
-    realStates = []
     fin = automata['final']
     autom = automata['automata']
     agrega = True
     tmp = []
 
+
+    # Revisar si antes de empezar tenemos Epsilons cerca
     while agrega:
         agrega = False
+        # Para cada estado en el que estamos posicionados
         for state in stand:
+            # Para cada linea del automata
             for ins in autom:
+                # Revisa si hay un epsilon enviando la posicion actual, de ser asi regresa el valor a donde te envia
                 res = ins.checkEpsilon(state.peek())
-                if res != None: #and res not in stand:
+                if res != None:
+                    # Para no meter valores repetidos -------
                     jump = False
                     for st in stand:
                         if st.peek() == res:
                             jump = True
                     if not jump:
+                        # Hacemos una copia del segundo camino para agregarlo
                         cop = copy.deepcopy(state)
                         cop.push(res)
                         stand.append(cop)
                         agrega = True
+                    # Para no meter valores repetidos -------
+    # Para cada caracter de la cadena
     for char in str:
+        # Para cada estado en el que estamos posicionados
         for state in stand:
+            # Para cada linea del automata
             for ins in autom:
+                # Revisa si hay una linea que te envia desde la posicion actual con el simbolo, de ser asi regresa el valor a donde te envia
                 res = ins.indicate(state.peek(), char)
                 if res != None:
+                    # Hacemos una copia del segundo camino para agregarlo
                     cop = copy.deepcopy(state)
                     cop.push(res)
                     tmp.append(cop)
@@ -40,41 +52,47 @@ def run(automata, str):
         agrega = True
         while agrega:
             agrega = False
+            # Para cada estado en el que estamos posicionados
             for state in stand:
+                # Para cada linea del automata
                 for ins in autom:
+                    # Revisa si hay un epsilon enviando la posicion actual, de ser asi regresa el valor a donde te envia
                     res = ins.checkEpsilon(state.peek())
-                    if res != None: #and res not in stand:
+                    if res != None:
+                        # Para no meter valores repetidos -------
                         jump = False
                         for st in stand:
                             if st.peek() == res:
                                 jump = True
                         if not jump:
-                            co = copy.deepcopy(state)
-                            co.push(res)
-                            stand.append(co)
+                            # Hacemos una copia del segundo camino para agregarlo
+                            cop = copy.deepcopy(state)
+                            cop.push(res)
+                            stand.append(cop)
                             agrega = True
+                        # Para no meter valores repetidos -------
 
 
-
+    # Limpiamos repetidos -----------
     for state in stand:
         if state not in tmp:
             tmp.append(state)
-
     stand = tmp
+    # Limpiamos repetidos -----------
 
-    print("Estados finales (donde se quedo despues de la solucion):")
-    for stack in stand:
-        print(stack.peek(), end=' ')
+
+    # print("Estados finales (donde se quedo despues de la solucion):")
+    # for stack in stand:
+    #     print(stack.peek(), end=' ')
 
     tmp = False
     print()
 
     for state in stand:
         if state.peek() in fin:
+            print("\nSi acepta")
             print(state)
             tmp = True
 
-    if tmp:
-        print("\nSi acepta")
-    else:
+    if not tmp:
         print("\nNo acepta")
--- user.py
+++ user.py
@@ -1,2 +1,2 @@
 def readUser():
-    return input("Ingresa la cadena de caracteres a probar en el automata:\n\n")
+    return input("Ingresa la cadena de caracteres a probar en el automata: ")
